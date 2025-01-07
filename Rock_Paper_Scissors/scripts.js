672da5e48190eb04ff41d96f428522ec5618bef3
Hristo Gospodinov <177312400+H-Gospodinov@users.noreply.github.com>
2025-01-07 23:57:51 +0200

    'Rock, Paper, Scissors': create dialog logic
    Create initial logic for the modal dialog elements.
    Reorder some of the previous code.

--- Rock_Paper_Scissors/scripts.js
+++ Rock_Paper_Scissors/scripts.js
@@ -1,16 +1,9 @@
 
 const gameButtons = [...document.querySelectorAll('.interface img')];
+const gameTrigger = document.querySelector('#gameTrigger');
 
 let humanChoice; // not set until click
 let computerChoice = Math.floor(Math.random() * gameButtons.length);
-
-gameButtons.forEach(function (thisButton) {
-    thisButton.addEventListener('click', function () {
-        this.classList.toggle('selected');
-        humanChoice = this.id;
-        displayHumanChoice();
-    });
-});
 
 function displayHumanChoice() {
     document.querySelector('#yourChoice strong').innerHTML = humanChoice;
@@ -18,5 +11,22 @@ function displayHumanChoice() {
 function displayComputerChoice() {
     document.querySelector('#theirChoice strong').innerHTML = gameButtons[computerChoice].id;
 }
+function toggleHidden(element, state) {
+    document.querySelector(element).hidden = state;
+}
 
-document.querySelector('#continue button').addEventListener('click', displayComputerChoice);
+gameButtons.forEach(function (thisButton) {
+    thisButton.addEventListener('click', function () {
+        this.classList.toggle('selected');
+        humanChoice = this.id;
+        displayHumanChoice();
+        toggleHidden('#gameTrigger', false);
+        toggleHidden('#theirChoice', true);
+    });
+});
+
+gameTrigger.addEventListener('click', function () {
+    displayComputerChoice();
+    this.hidden = true;
+    toggleHidden('#theirChoice', false);
+});
