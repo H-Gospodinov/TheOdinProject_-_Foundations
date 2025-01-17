28d1d8cceb625fae5a1aa128c8566179b730e8b8
Hristo Gospodinov <177312400+H-Gospodinov@users.noreply.github.com>
2025-01-17 02:08:31 +0200

    "Etch a Sketch": create grid logic
    Create variable grid resolution that can be set by users.
    Deafult grid size is 16 x 16 cells.

--- Etch_Sketch/scripts.js
+++ Etch_Sketch/scripts.js
@@ -1,8 +1,43 @@
 
 const grid = document.querySelector('#grid');
-const cells = 16 * 16;
+const setGrid = document.querySelector('#setGrid');
+const setGridTxt = 'Set the number of either horizontal or vertical cells. \nMaximum allowed is 64.';
 
-for (let i = 1; i <= cells; i++) {
-    const gridCell = document.createElement('div');
-    grid.appendChild(gridCell).classList.add('grid-cell');
+let gridSize = 16; // default grid size is 16 x 16 cells
+
+function createCells(number, size) {
+
+    for (let i = 1; i <= number; i++) {
+
+        const gridCell = document.createElement('div');
+
+        gridCell.classList.add('grid-cell');
+        grid.appendChild(gridCell);
+    }
+    grid.style.setProperty('--cell-size', size +'%');
 }
+createCells(gridSize ** 2, 100 / gridSize);
+
+function resetGrid(newSize) {
+
+    grid.replaceChildren(); // remove existing cells
+    gridSize = newSize; // set new size
+    createCells(gridSize ** 2, 100 / gridSize);
+}
+
+function userInput(input) {
+    if (input == null) {
+        return;
+    }
+    else if (!+input || input < 1 || input > 64) {
+        alert('Please enter a number between 1 and 64');
+        userInput(prompt(setGridTxt));
+    }
+    else {
+        resetGrid(parseInt(input));
+    }
+}
+setGrid.addEventListener('click', function () {
+
+    userInput(prompt(setGridTxt));
+});
