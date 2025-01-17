d458265068eb3d61d7926016b7e780c0fe484ed7
Hristo Gospodinov <177312400+H-Gospodinov@users.noreply.github.com>
2025-01-17 22:01:53 +0200

    "Etch a Sketch": add touch support
    Extend the colorization logic to support mobile device touchmove events.

--- Etch_Sketch/scripts.js
+++ Etch_Sketch/scripts.js
@@ -2,6 +2,7 @@
 const grid = document.querySelector('#grid');
 const setGrid = document.querySelector('#setGrid');
 const setGridTxt = 'Set the number of either horizontal or vertical cells. \nMaximum allowed is 64.';
+const randomColor = () => Math.floor(Math.random()*16777215).toString(16); // HEX
 
 let gridSize = 16; // default grid size is 16 x 16 cells
 
@@ -44,12 +45,15 @@ setGrid.addEventListener('click', () => {
 
 grid.addEventListener('mouseover', (event) => {
 
-    const randomColor = Math.floor(Math.random()*16777215).toString(16); // HEX
-    event.target.style.setProperty('background-color', '#'+randomColor);
+    event.target.style.setProperty('background-color', '#'+randomColor());
 });
-/* grid.addEventListener('mouseout', (event) => {
 
-    setTimeout(function () {
-        event.target.removeAttribute('style')
-    }, 10000);
-}); */
+grid.addEventListener("touchmove", (event) => {
+
+    const touch = event.touches[0]; // get the touch element
+    const touchCell = document.elementFromPoint(touch.clientX, touch.clientY); // get the DOM element
+
+    if (touchCell && touchCell.className == 'grid-cell') {
+        touchCell.style.setProperty('background-color', '#'+randomColor());
+    }
+});
